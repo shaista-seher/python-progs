5d7844f2749a4c4959bc9f47306b865df07d189e
shaista seher <shaistaarshad006@gmail.com>
2025-11-16 15:04:07 +0530

    Create setoperations.py

---
+++ setoperations.py
@@ -0,0 +1,11 @@
+# Python Program to Illustrate Different Set Operations
+
+# Defining two sets
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+
+# Performing set operations
+print("Union of E and N is", E.union(N))
+print("Intersection of E and N is", E.intersection(N))
+print("Difference of E and N is", E.difference(N))
+print("Symmetric difference of E and N is", E.symmetric_difference(N))
